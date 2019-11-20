6c7cc3b47f139d95cffb80b2196b810505e6452b
Suhan Moon <lunyx@me.com>
2019-11-20 17:32:32 +0900

    source reactory

---
+++ android/src/main/java/io/actbase/kakaosdk/navi/ARNKakaoNavi.java
@@ -0,0 +1,27 @@
+package io.actbase.kakaosdk.navi;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.plusfriend.PlusFriendService;
+
+public class ANKakaoNavi extends ReactContextBaseJavaModule {
+
+    private ReactApplicationContext reactContext;
+
+    public ANKakaoNavi(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "ANKakaoNavi";
+    }
+
+}
