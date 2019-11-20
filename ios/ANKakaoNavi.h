493ab72465f09d8aec4e786ebf05f07d56c86795
Suhan Moon <lunyx@me.com>
2019-11-20 16:36:37 +0900

    source refactory

--- ios/ANKakaoNavi.h
+++ ios/ANKakaoNavi.h
@@ -1,17 +1,17 @@
 //
-//  ANKakaoChannel.h
+//  ANKakaoLink.h
 //  RNCKakaoSDK
 //
-//  Created by Minhyuk Kim on 2019/11/18.
+//  Created by Suhan Moon on 2019/11/03.
 //  Copyright © 2019 Facebook. All rights reserved.
 //
-
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #else
 #import <React/RCTBridgeModule.h>
 #endif
 
-@interface ANKakaoChannel : NSObject <RCTBridgeModule>
+@interface ARNKakaoNavi : NSObject <RCTBridgeModule>
 
 @end
+  
---
+++ ios/ANKakaoNavi.m
@@ -0,0 +1,21 @@
+//
+//  ANKakaoLink.m
+//  RNCKakaoSDK
+//
+//  Created by Suhan Moon on 2019/11/03.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "ARNKakaoNavi.h"
+
+@implementation ARNKakaoNavi
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+RCT_EXPORT_MODULE(ARNKakaoNavi)
+
+
+@end
