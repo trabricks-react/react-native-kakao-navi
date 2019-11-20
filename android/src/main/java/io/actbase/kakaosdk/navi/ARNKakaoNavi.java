93a1b47e0018d5e36556f0bbf420b24051270d51
Suhan Moon <lunyx@me.com>
2019-11-20 17:36:40 +0900

    source reactory

--- android/src/main/java/io/actbase/kakaosdk/navi/ARNKakaoNavi.java
+++ android/src/main/java/io/actbase/kakaosdk/navi/ARNKakaoNavi.java
@@ -6,8 +6,6 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.kakao.util.exception.KakaoException;
-import com.kakao.plusfriend.PlusFriendService;
 
 public class ARNKakaoNavi extends ReactContextBaseJavaModule {
 
