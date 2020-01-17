8fcb958e4a5daa345570af23db6b471c2e4a4d6a
zusinShinpei <kmh011225@naver.com>
2020-01-17 23:35:20 +0900

    Fix: example

--- example/App.js
+++ example/App.js
@@ -6,106 +6,63 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TouchableOpacity, ActivityIndicator} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
 
-import KakaoSDK from 'react-native-ccs-kakaosdk';
+import KakaoSDK from "react-native-ccs-kakaosdk";
 
-type Props = {};
-export default class App extends Component<Props> {
+export default function App() {
+  const share = () => {
+    const destination = {
+      name: "카카오판교오피스",
+      x: 321286,
+      y: 533707
+    };
+    KakaoSDK.navi.share(destination);
+  };
 
-	state = {
+  const navigate = () => {
+    const destination = {
+      name: "카카오판교오피스",
+      x: 321286,
+      y: 533707
+    };
+    KakaoSDK.navi.navigate(destination);
+  };
 
-	};
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => share()} style={styles.button}>
+        <Text>share</Text>
+      </TouchableOpacity>
 
-	constructor(props) {
-		super(props);
-	}
-
-	login() {
-		this.setState({ loading: true });
-
-		KakaoSDK.login().then((response) => {
-			console.log(response);
-			this.setState({ loading: false });
-		}).catch(e => {
-			console.log(e);
-			this.setState({ loading: false });
-		});
-	}
-
-	async logout() {
-		this.setState({ loading: true });
-
-		KakaoSDK.logout().then((response) => {
-			console.log(response);
-			this.setState({ loading: false });
-		}).catch(e => {
-			console.log(e);
-			this.setState({ loading: false });
-		});
-	}
-
-	async getToken() {
-		this.setState({ loading: true });
-
-		KakaoSDK.getAccessToken().then(token => {
-			alert(JSON.stringify(token));
-			this.setState({ loading: false });
-		}).catch(e => {
-			console.log(e);
-			this.setState({ loading: false });
-
-		});
-	}
-		
-	render() {
-		return (
-			<View style={styles.container}>
-
-				{(this.state.loading && true) && (
-				<View style={ styles.loading }>
-					<ActivityIndicator />
-				</View>
-				)}
-
-				<TouchableOpacity onPress={() => this.login() } style={ styles.button }>
-					<Text>Login</Text>
-				</TouchableOpacity>
-
-				<TouchableOpacity onPress={() => this.logout() } style={ styles.button }>
-					<Text>Logout</Text>
-				</TouchableOpacity>
-
-				<TouchableOpacity onPress={() => this.getToken() } style={ styles.button }>
-					<Text>getToken</Text>
-				</TouchableOpacity>
-			</View>
-		);
-	}
+      <TouchableOpacity onPress={() => navigate()} style={styles.button}>
+        <Text>navigate</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
 
-	loading : {
-		...StyleSheet.absoluteFill, 
-		zIndex: 10, 
-		backgroundColor: 'rgba(0,0,0,0.8)', 
-		alignItems: 'center', 
-		justifyContent: 'center'
-	},
-
-	button: {
-		borderColor: '#000', 
-		borderWidth: 1, 
-		padding: 10, 
-		width: 200, 
-		margin: 10,
-	}
+  button: {
+    borderColor: "#000",
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
+    margin: 10
+  }
 });
